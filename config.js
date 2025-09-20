eb3dd46c168e6e27750a11168d063f6be73c4482
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2025-09-20 18:44:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~WggCFZ4Y#DmiEGoBaA93-etVwJalYGmiLrFDFYHpJW9MuN8B5UNM",
+SESSION_ID: process.env.SESSION_ID || "DmiEGoBaA93-etVwJalYGmiLrFDFYHpJW9MuN8B5UNM",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
